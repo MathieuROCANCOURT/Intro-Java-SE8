25c16f0ab4d8940cc3f59350183a96a511b31759
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-07-31 11:10:11 +0200

    docs: Add docstring to the GameMysteryNumber class.

--- BaseTP1Game/src/GameMysteryNumber.java
+++ BaseTP1Game/src/GameMysteryNumber.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
 
+/**
+ * Play this game, in which the user must choose a number between 1 and 100 and
+ * guess the hidden number. The user can play multiple rounds.
+ * 
+ * @author RocancourtM
+ */
 public class GameMysteryNumber {
 	private static boolean play(boolean start, Scanner sc) {
 		String display;
-		if (start) display = "Bonjour! Souhaitez-vous jouer [o/oui/n/non]?";
-		else display = "Voulez-vous rejouer [o/oui/n/non]?";
-		
-		while(true) {
+		if (start)
+			display = "Bonjour! Souhaitez-vous jouer [o/oui/n/non]?";
+		else
+			display = "Voulez-vous rejouer [o/oui/n/non]?";
+
+		while (true) {
 			System.out.println(display);
 			String input = sc.nextLine().toLowerCase();
-			if (input.equals("o") || input.equals("oui")) return true;
+			if (input.equals("o") || input.equals("oui"))
+				return true;
 			if (input.equals("n") || input.equals("non")) {
 				System.out.println("En revoir!");
 				return false;
@@ -18,16 +27,20 @@ public class GameMysteryNumber {
 	}
 
 	public static void main(String[] args) {
-		boolean first = true; Scanner sc = new Scanner(System.in);
-		
+		boolean first = true;
+		Scanner sc = new Scanner(System.in);
+
 		while (play(first, sc)) {
 			first = false;
-			int genNumber = (int)(Math.random() * 100) + 1, inputNumber = 0, nbTurn = 0;
-			
+			int genNumber = (int) (Math.random() * 100) + 1, inputNumber = 0, nbTurn = 0;
+
 			while (genNumber != inputNumber) {
-				if (nbTurn == 0) System.out.print("Saisir une valeur entre 1 et 100 inclus:");
-				else if (genNumber > inputNumber) System.out.print("Saisir une valeur plus grande:");
-				else System.out.print("Saisir une valeur plus petite:");
+				if (nbTurn == 0)
+					System.out.print("Saisir une valeur entre 1 et 100 inclus:");
+				else if (genNumber > inputNumber)
+					System.out.print("Saisir une valeur plus grande:");
+				else
+					System.out.print("Saisir une valeur plus petite:");
 				try {
 					inputNumber = Integer.parseInt(sc.nextLine());
 					nbTurn++;
