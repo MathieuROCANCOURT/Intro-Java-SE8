25d31677f5c9bc9ca2b141c2cf4ba26622f7a4ca
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-03 08:02:01 +0200

    refactor: Remove unnecessary lines of code.

--- BaseEx6Operation/src/Triangle.java
+++ BaseEx6Operation/src/Triangle.java
@@ -14,17 +14,11 @@ public class Triangle {
 	private static void showTriangle(int triangleHeight) {
 		if (triangleHeight < 1) {
 			System.err.println("La hauteur du triangle doit être un entier positif.");
-		} else {
+		} else {			
 			for (int line = 1; line <= triangleHeight; line++) {
-				double roundedInt = Math.ceil((double) triangleHeight / 2) * 2;
+				char[] rowStar = new char[triangleHeight * 2 - 1];
 
-				if (triangleHeight % 2 != 0) {
-					roundedInt -= 1;
-				}
-
-				char[] rowStar = new char[(int) roundedInt * 2 - 1];
-
-				for (int index = (int) roundedInt - line; index < roundedInt + line - 1; index++) {
+				for (int index = triangleHeight - line; index < triangleHeight + line - 1; index++) {
 					rowStar[index] = '*';
 				}
 				System.out.println(rowStar);
@@ -38,6 +32,6 @@ public class Triangle {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		showTriangle(8);
+		showTriangle(9);
 	}
 }
